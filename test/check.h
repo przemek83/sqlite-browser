5054b15546b206fbcb253358b6f2d31b1de38f75
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-11-08 16:03:51 +0100

    Create more meneingfull test. Remove not needed Qt dependencies from tests.

--- test/check.h
+++ test/check.h
@@ -1,11 +1,17 @@
 #include <QTest>
 
+#include <QFile>
+
 class MyTest : public QObject
 {
     Q_OBJECT
 
 private slots:
-    void testFunction1() { QVERIFY2(true, "Test failed"); }
+    void testQFile()
+    {
+        QFile file("check.txt");
+        QVERIFY2(file.fileName() == "check.txt", "Test failed");
+    }
 };
 
 QTEST_MAIN(MyTest)
