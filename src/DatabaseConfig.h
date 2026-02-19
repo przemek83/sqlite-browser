c4a52d43370180bcb6c6d92b383e0c4cddad44b0
przemek83 <4788832+przemek83@users.noreply.github.com>
2026-02-19 12:11:02 +0100

    Make DatabaseConfig movable.

--- src/DatabaseConfig.h
+++ src/DatabaseConfig.h
@@ -37,14 +37,14 @@ private:
         const bool primaryKey_;
     };
 
-    const QMap<DbColumnType, QString> typeToStringMap_{
+    QMap<DbColumnType, QString> typeToStringMap_{
         {DatabaseConfig::DbColumnType::INTEGER, "INTEGER"},
         {DatabaseConfig::DbColumnType::REAL, "REAL"},
         {DatabaseConfig::DbColumnType::TEXT, "VARCHAR(50)"}};
 
-    const QString tableName_{QStringLiteral("client")};
+    QString tableName_{QStringLiteral("client")};
 
-    const std::vector<SqlColumn> columns_{
+    std::vector<SqlColumn> columns_{
         {"id", "id", DatabaseConfig::DbColumnType::INTEGER, true},
         {"firstname", "First name", DatabaseConfig::DbColumnType::TEXT, false},
         {"lastname", "Last name", DatabaseConfig::DbColumnType::TEXT, false},
